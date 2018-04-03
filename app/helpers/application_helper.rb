4d9a06774a1c739894d932a0e39c7c366c9e118c
lynnfalkner <linnf@live.com>
2018-04-03 03:13:25 +0000

    Customized view for users and non users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  def login_helper style = ''
+   if user_signed_in?
+    (link_to current_user.name, edit_user_registration_path, class: style) + " ".html_safe +
+    (link_to "Logout", destroy_user_session_path, method: :delete, class: style)
+   else
+     (link_to "Register", new_user_registration_path, class: style) + " ".html_safe + 
+     (link_to "Login", new_user_session_path, class: style)
+   end
+  end
+  
 end
